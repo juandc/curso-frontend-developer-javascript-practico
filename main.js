bb92f99274a1b425aa7c04f2ea530f05d75bd7e6
David Garzon <garzondavidsorrento@gmail.com>
2022-09-22 14:25:09 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,91 +1,75 @@
 const menuEmail = document.querySelector('.navbar-email');
-const menuHamIcon = document.querySelector('.menu');
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
-const productDetailContainer = document.querySelector('#productDetail');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
-productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+  const isAsideClosed = aside.classList.contains('inactive');
 
-  if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive');
+  if(!isAsideClosed){
+    aside.classList.add('inactive')
   }
-  
+
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+  const isAsideClosed = aside.classList.contains('inactive');
 
-  if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive'); 
+  if(!isAsideClosed){
+    aside.classList.add('inactive')
   }
-
-  closeProductDetailAside();
-  
   mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-  
-  if (!isMobileMenuClosed) {
-    mobileMenu.classList.add('inactive');
+  const isDesktopclosed = desktopMenu.classList.contains('inactive');
+
+  if(!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive')
+  }
+  if(!isDesktopclosed){
+    desktopMenu.classList.add('inactive')
   }
 
-  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
-  
-  if (!isProductDetailClosed) {
-    productDetailContainer.classList.add('inactive'); 
-  }
-  
-  shoppingCartContainer.classList.toggle('inactive');
+  aside.classList.toggle('inactive');
 }
 
-function openProductDetailAside() {
-  shoppingCartContainer.classList.add('inactive');
-  productDetailContainer.classList.remove('inactive');
-}
-
-function closeProductDetailAside() {
-  productDetailContainer.classList.add('inactive');
-}
-
-const productList = [];
+const productList =[];
 productList.push({
-  name: 'Bike',
+  name: 'bike',
   price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 productList.push({
-  name: 'Pantalla',
+  name: 'Televisor',
   price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 productList.push({
-  name: 'Compu',
+  name: 'Computador',
   price: 620,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
-function renderProducts(arr) {
-  for (product of arr) {
+// Con el array listo. solo resta ingresar los datos en el HTML, eso, recorriendo el array
+// con un ciclo for of
+
+function renderProducts (arr) {
+  for (const product of arr) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
   
-    // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
-    productImg.addEventListener('click', openProductDetailAside);
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -93,7 +77,7 @@ function renderProducts(arr) {
     const productInfoDiv = document.createElement('div');
   
     const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
+    productPrice.innerText = '$'+ product.price;
     const productName = document.createElement('p');
     productName.innerText = product.name;
   
@@ -101,19 +85,25 @@ function renderProducts(arr) {
     productInfoDiv.appendChild(productName);
   
     const productInfoFigure = document.createElement('figure');
-    const productImgCart = document.createElement('img');
-    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    const productImgCard = document.createElement('img');
+    productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
   
-    productInfoFigure.appendChild(productImgCart);
+    // ahora, vamos a meter cada variable dentro de su variable padre
   
+    productInfoFigure.appendChild(productImgCard);
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(productInfoFigure);
-  
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
   
+    // en la siquiente linea de codigo se agrega el producto ya maquetado(productCard) a la const cardsContainer, que es la que permitira dibujar todo el codigo maquetado en HTML.
+  
     cardsContainer.appendChild(productCard);
+    
+  
   }
 }
 
 renderProducts(productList);
+// product es el objeto que recorre cada uno de los elementos en el array, para crear con document.createElement
+// cada uno de los elementos del HTML, pero en JS.
