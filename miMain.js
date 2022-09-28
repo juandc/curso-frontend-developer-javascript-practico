392587feb6210395a0dc83682b40a80499132514
Felipe Molano <molano5858@gmail.com>
2022-09-27 21:55:14 -0500

    Avance clase 20

--- miMain.js
+++ miMain.js
@@ -3,13 +3,44 @@ let desktopMenu=document.querySelector('.desktop-menu');
 let mobileMenu=document.querySelector('.mobile-menu');
 let menuHamburguesa=document.querySelector('.menu');
 
+let shoppingCar=document.querySelector('.navbar-shopping-cart')
+let productDetail=document.getElementsByClassName('product-detail')
+
 navBarEmail.addEventListener('click',mostrarYQuitarMenu);
 menuHamburguesa.addEventListener('click',mostrarYQuitarMenuMobile);
+shoppingCar.addEventListener('click',mostrarYQuitarProductDetail)
 
 function mostrarYQuitarMenu(){
+    let isProductDetailOpen= !productDetail[0].classList.contains('inactivo');
+
+    if(isProductDetailOpen){
+        productDetail[0].classList.add('inactivo')
+    }
+
     desktopMenu.classList.toggle('inactivo')
 }
 
 function mostrarYQuitarMenuMobile(){
+    let isProductDetailOpen= !productDetail[0].classList.contains('inactivo');
+
+    if(isProductDetailOpen){
+        productDetail[0].classList.add('inactivo')
+    }
+
     mobileMenu.classList.toggle('inactivo')
+}
+
+function mostrarYQuitarProductDetail(){
+    let isMobileMenuOpen= !mobileMenu.classList.contains('inactivo');
+    let isDesktopMenuOpen= !desktopMenu.classList.contains('inactivo');
+
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add('inactivo')
+    }
+
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add('inactivo')
+    }
+
+    productDetail[0].classList.toggle('inactivo')
 }
