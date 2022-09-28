26f2e7db237c3e6b3a3f4009da9cc89a0fb29c0a
Felipe Molano <molano5858@gmail.com>
2022-09-28 17:58:43 -0500

    Avances clase 23

--- miMain.js
+++ miMain.js
@@ -10,23 +10,40 @@ navBarEmail.addEventListener('click',mostrarYQuitarMenu);
 menuHamburguesa.addEventListener('click',mostrarYQuitarMenuMobile);
 shoppingCar.addEventListener('click',mostrarYQuitarProductDetail);
 
-let cardsContainer=document.querySelector('.cards-container')
+let cardsContainer=document.querySelector('.cards-container');
+
+let productDetail2=document.querySelector('.product-detail-2');
+let closeProductDetail2=document.querySelector('.product-detail-close')
+
 
 function mostrarYQuitarMenu(){
     let isProductDetailOpen= !productDetail[0].classList.contains('inactivo');
+    let isProductDetail2Open= !productDetail2.classList.contains('inactivo');
 
     if(isProductDetailOpen){
-        productDetail[0].classList.add('inactivo')
+        productDetail[0].classList.add('inactivo');
     }
+
+    if(isProductDetail2Open){
+        productDetail2.classList.add('inactivo');
+    }
+    
+
+    
 
     desktopMenu.classList.toggle('inactivo')
 }
 
 function mostrarYQuitarMenuMobile(){
     let isProductDetailOpen= !productDetail[0].classList.contains('inactivo');
+    let isProductDetail2Open= !productDetail2.classList.contains('inactivo');
 
     if(isProductDetailOpen){
         productDetail[0].classList.add('inactivo')
+    }
+
+    if(isProductDetail2Open){
+        productDetail2.classList.add('inactivo')
     }
 
     mobileMenu.classList.toggle('inactivo')
@@ -35,6 +52,7 @@ function mostrarYQuitarMenuMobile(){
 function mostrarYQuitarProductDetail(){
     let isMobileMenuOpen= !mobileMenu.classList.contains('inactivo');
     let isDesktopMenuOpen= !desktopMenu.classList.contains('inactivo');
+    let isProductDetail2Open= !productDetail2.classList.contains('inactivo');
 
     if(isDesktopMenuOpen){
         desktopMenu.classList.add('inactivo')
@@ -44,7 +62,30 @@ function mostrarYQuitarProductDetail(){
         mobileMenu.classList.add('inactivo')
     }
 
+    if(isProductDetail2Open){
+        productDetail2.classList.add('inactivo')
+    }
+
     productDetail[0].classList.toggle('inactivo')
+}
+
+function mostrarProductDetail2(){
+    let isProductDetailOpen= !productDetail[0].classList.contains('inactivo');
+    let isDesktopMenuOpen=!desktopMenu.classList.contains('inactivo');
+
+    if(isProductDetailOpen){
+        productDetail[0].classList.add('inactivo');
+    }
+
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add('inactivo');
+    }
+
+    productDetail2.classList.remove('inactivo');
+}
+
+function closeProductDetailModal(){
+    productDetail2.classList.add('inactivo')
 }
 
 
@@ -79,6 +120,9 @@ for (parametro of productList){
     const productImg = document.createElement('img');
     productImg.setAttribute('src', parametro.image);
 
+    //creacion del listener para que cuando le demos click a cada imagen nos abra el detalle del producto
+    productImg.addEventListener('click', mostrarProductDetail2)
+
     const productInfo=document.createElement('div');
     productInfo.classList.add('product-info');
 
@@ -97,6 +141,8 @@ for (parametro of productList){
     const productInfoFigure= document.createElement('figure');
     const productInfoFigureImg= document.createElement('img');
     productInfoFigureImg.setAttribute('src',"./icons/bt_add_to_cart.svg")
+
+    closeProductDetail2.addEventListener('click', closeProductDetailModal)
 
     productInfoFigure.appendChild(productInfoFigureImg);
     productCard.appendChild(productImg);
