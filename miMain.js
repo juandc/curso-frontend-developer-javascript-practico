0db1a4e756bcc361d87084d0db77865e281f61db
Felipe Molano <molano5858@gmail.com>
2022-09-28 16:34:01 -0500

    Avances clase 21

--- miMain.js
+++ miMain.js
@@ -8,7 +8,9 @@ let productDetail=document.getElementsByClassName('product-detail')
 
 navBarEmail.addEventListener('click',mostrarYQuitarMenu);
 menuHamburguesa.addEventListener('click',mostrarYQuitarMenuMobile);
-shoppingCar.addEventListener('click',mostrarYQuitarProductDetail)
+shoppingCar.addEventListener('click',mostrarYQuitarProductDetail);
+
+let cardsContainer=document.querySelector('.cards-container')
 
 function mostrarYQuitarMenu(){
     let isProductDetailOpen= !productDetail[0].classList.contains('inactivo');
@@ -44,3 +46,64 @@ function mostrarYQuitarProductDetail(){
 
     productDetail[0].classList.toggle('inactivo')
 }
+
+
+let productList=[];
+//Creando los productos manualmente
+productList.push({
+    name:'Bike',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name:'Bike 2',
+    price:220,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name:'Bike 3',
+    price:320,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+for (parametro of productList){
+    const productCard= document.createElement('div');
+    productCard.classList.add('product-card');
+
+    
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', parametro.image);
+
+    const productInfo=document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    
+
+    const productNameAndPriceContainer= document.createElement('div');
+    const productPrice=document.createElement('p');
+    productPrice.innerText= '$'+ parametro.price;
+    const productName=document.createElement('p');
+    productName.innerText= parametro.name;
+
+    productNameAndPriceContainer.appendChild(productPrice);
+    productNameAndPriceContainer.appendChild(productName);
+
+
+    const productInfoFigure= document.createElement('figure');
+    const productInfoFigureImg= document.createElement('img');
+    productInfoFigureImg.setAttribute('src',"./icons/bt_add_to_cart.svg")
+
+    productInfoFigure.appendChild(productInfoFigureImg);
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+    productInfo.appendChild(productNameAndPriceContainer);
+    productInfo.appendChild(productInfoFigure);
+
+
+
+    cardsContainer.appendChild(productCard)
+     
+};
+
