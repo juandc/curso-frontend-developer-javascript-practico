e6880a86741b3369bfc3df3ff697b5c39d46bf47
Felipe Molano <molano5858@gmail.com>
2022-09-27 21:09:42 -0500

    Avances clase 19

--- miMain.js
+++ miMain.js
@@ -1,8 +1,15 @@
 let navBarEmail=document.querySelector('.navbar-email');
-let desktopMenu=document.querySelector('.desktop-menu')
+let desktopMenu=document.querySelector('.desktop-menu');
+let mobileMenu=document.querySelector('.mobile-menu');
+let menuHamburguesa=document.querySelector('.menu');
 
 navBarEmail.addEventListener('click',mostrarYQuitarMenu);
+menuHamburguesa.addEventListener('click',mostrarYQuitarMenuMobile);
 
 function mostrarYQuitarMenu(){
     desktopMenu.classList.toggle('inactivo')
+}
+
+function mostrarYQuitarMenuMobile(){
+    mobileMenu.classList.toggle('inactivo')
 }
