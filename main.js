795ea5bb369ee4bda97a8ec0d3442f525c31c27c
cmtorresa <cesarmta87@gmail.com>
2022-10-01 23:10:04 -0500

    Mostrar el otro menú del modo mobile, media query para los dos menus desplegables

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail =document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menHamIcon = document.querySelector(".menu");
 
+menHamIcon.addEventListener("click", aparecerMenuMobil);
 menuEmail.addEventListener("click", aparecerMenu);
 
 function aparecerMenu() {
 desktopMenu.classList.toggle("inactive");
 }
+
+function aparecerMenuMobil() {
+  mobileMenu.classList.toggle("inactive");
+  }
