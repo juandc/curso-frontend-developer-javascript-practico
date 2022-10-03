a87d59ebc41ca22bd806e79e376c9b4d3a270714
cmtorresa <cesarmta87@gmail.com>
2022-10-03 14:19:05 -0500

    Trabajo en el carrito de compras. muestra del menú y del carrito sin interrupir pantalla.

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const menuEmail =document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const carritoCompras = document.querySelector(".product-detail");
 
 menHamIcon.addEventListener("click", aparecerMenuMobil);
 menuEmail.addEventListener("click", aparecerMenu);
+menuCarritoIcon.addEventListener("click", aparecerMenuCarrito);
 
 function aparecerMenu() {
-desktopMenu.classList.toggle("inactive");
+  desktopMenu.classList.toggle("inactive");
+  carritoCompras.classList.add("inactive");
 }
 
 function aparecerMenuMobil() {
   mobileMenu.classList.toggle("inactive");
-  }
+  carritoCompras.classList.add("inactive");
+}
+
+function aparecerMenuCarrito() {
+  carritoCompras.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+}
