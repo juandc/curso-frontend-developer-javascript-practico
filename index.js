313d06ed48dd331f875a52e76a469b3942701230
Gabriel Oquendo <oquendogabriel18@gamil.com>
2022-11-05 21:33:47 -0500

    Integrate mobile menu

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const listeningEmail = document.querySelector(".navbar-email");
 const destokpChange = document.querySelector(".desktop-menu");
 
+const listeningMenu = document.querySelector(".menu");
+const mobileChange = document.querySelector(".mobile-menu");
+
+const ChangeMobile = () => {
+  mobileChange.classList.toggle("inactive");
+};
+
 const Change = () => {
   destokpChange.classList.toggle("inactive");
 };
 
 listeningEmail.addEventListener("click", Change);
+listeningMenu.addEventListener("click", ChangeMobile);
