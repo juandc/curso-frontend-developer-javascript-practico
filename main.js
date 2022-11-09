e5eaa3d72cbb72fd51b2517d09f08e8828e00715
Juan17Informatico <juancampuzano2356@gmail.com>
2022-11-08 21:34:19 -0500

    Realizar carrito de compras. Asegurar su funcionamiento en version mobile y con el desktop menu, aplicando logica de programacion con js

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
   // desktopMenu.classList.remove('inactive');
+  const isAsideClosed = aside.classList.contains('inactive');
+  
+  if (!isAsideClosed){
+    aside.classList.add('inactive');
+  }
+
   desktopMenu.classList.toggle('inactive');
+
+
 }
 
 function toggleMobileMenu(){
+  const isAsideClosed = aside.classList.contains('inactive');
+  
+  if (!isAsideClosed){
+    aside.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  
+  // console.log(isMobileMenuClosed);
+  // aside.classList.toggle('inactive');
+
+  if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+  }
+  
+  if(!isDesktopMenuClosed){
+    desktopMenu.classList.add('inactive');
+  }
+
+
+  aside.classList.toggle('inactive');
+
 }
