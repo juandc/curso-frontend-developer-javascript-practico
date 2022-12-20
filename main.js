724cbab1f34be13bf582be94e629266b9fcdcbfe
Yeison Andres Ospitia <yeison-0494@hotmail.com>
2022-12-20 05:07:00 -0500

    clase array minuto 18:30

--- main.js
+++ main.js
@@ -27,3 +27,54 @@ function toggleshoppingcart() {
     desktopmenu.classList.add("inactive");
     mobilmenu.classList.add("inactive");
  }
+
+ const productlist = []; 
+ productlist.push({
+    name: "bike",
+    price: "120",
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ });
+
+ productlist.push({
+    name: "Compu",
+    price: "1200",
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ });
+
+ productlist.push({
+    name: "Casco",
+    price: "20",
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ });
+
+ productlist.push({
+    name: "Portatil",
+    price: "800",
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+ });
+
+ for (product of productlist) {
+    const productcard=document.createElement("div");
+    productcard.classList.add("product-card");
+
+    const img=document.createElement("img");
+    img.setAttribute("src", product.image);
+
+    const productinfo=document.createElement("div");
+    productinfo.classList.add("product-info");
+
+    const productinfodiv=document.createElement("div");
+    
+    const productinfoname=document.createElement("p");
+    productinfoname.innerText=product.name
+
+    const productinfoprice=document.createElement("p");
+    productinfoprice.innerText= "$" + product.price;
+
+    const figure=document.createElement("Figure");
+
+    const logo=document.createElement("img");
+    logo.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+
+ }
