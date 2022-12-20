adfe4fdd2a12227144d38d5e0b5e505595869a7d
YeisonOspitia <118226100+YeisonOspitia@users.noreply.github.com>
2022-12-20 11:39:47 -0500

    Update main.js
    actualizar manual clase array

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuhamicon= document.querySelector(".menu");
 const mobilmenu= document.querySelector(".mobile-menu");
 const productdetail= document.querySelector(".product-detail");
 const shoppingcart= document.querySelector(".navbar-shopping-cart");
+const cardscontainer= document.querySelector(".cards-container");
 
 
 menuemail.addEventListener('click', toggledesktopmenu);
@@ -52,29 +53,44 @@ function toggleshoppingcart() {
     price: "800",
     image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
  });
-
- for (product of productlist) {
-    const productcard=document.createElement("div");
-    productcard.classList.add("product-card");
-
-    const img=document.createElement("img");
-    img.setAttribute("src", product.image);
-
-    const productinfo=document.createElement("div");
-    productinfo.classList.add("product-info");
-
-    const productinfodiv=document.createElement("div");
-    
-    const productinfoname=document.createElement("p");
-    productinfoname.innerText=product.name
-
-    const productinfoprice=document.createElement("p");
-    productinfoprice.innerText= "$" + product.price;
-
-    const figure=document.createElement("Figure");
-
-    const logo=document.createElement("img");
-    logo.setAttribute("src", "./icons/bt_add_to_cart.svg");
-
-
+ 
+ function renderproducts(arr) {
+   for (product of arr) {
+      const productcard=document.createElement("div");
+      productcard.classList.add("product-card");    
+  
+      const Pimg=document.createElement("img");
+      Pimg.setAttribute("src", product.image);
+  
+      const productinfo=document.createElement("div");
+      productinfo.classList.add("product-info");
+  
+      const productinfodiv=document.createElement("div");
+      
+      const productinfoname=document.createElement("p");
+      productinfoname.innerText=product.name;
+      const productinfoprice=document.createElement("p");
+      productinfoprice.innerText= "$" + product.price;
+  
+      productinfodiv.appendChild(productinfoprice);
+      productinfodiv.appendChild(productinfoname);
+  
+  
+      const productinfofigure=document.createElement("Figure");
+      const productimgcart=document.createElement("img");
+      productimgcart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+  
+      productinfofigure.appendChild(productimgcart); 
+  
+      productinfo.appendChild(productinfodiv);
+      productinfo.appendChild(productinfofigure);
+  
+      productcard.appendChild(Pimg);
+      productcard.appendChild(productinfo);
+  
+      cardscontainer.appendChild(productcard);
+  
+   }
  }
+
+ renderproducts(productlist);
