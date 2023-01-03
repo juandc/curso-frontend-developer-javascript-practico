0e3dd19bc9f85ab4df225c1aa1c34785410ac15f
molixx13 <molinak0517@gmail.com>
2023-01-03 06:50:39 -0500

    se añadio el mobile menu y se añadio interaccion a los menus

--- javascript🦗.js
+++ javascript🦗.js
@@ -1,7 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburgesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamburgesa.addEventListener('click',toggleMobileMenu);
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('innactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('innactive');
 }
