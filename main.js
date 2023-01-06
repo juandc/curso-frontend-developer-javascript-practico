42f96c858644b802e7ef81657b56235eafee1252
peves-platzi <mjpeves2002@gmail.com>
2023-01-06 10:45:47 -0500

    Lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var menuHamburguer = document.querySelector('.menu');
 var mobileMenu = document.querySelector('.mobile-menu')
 var menuCarrito = document.querySelector('.navbar-shopping-cart');
 var aside = document.querySelector('.product-detail');
+var cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguer.addEventListener('click', toggleMobileMenu);
@@ -34,3 +36,76 @@ function toggleCarritoAside(){
         aside.classList.toggle('inactive');
 }
 
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Bike 2',
+    price: 150,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'Bike 3',
+    price: 200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+renderProducts(productList);
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+     
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+     
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+     
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = 'S/' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+     
+        //Insertar p precio y p name en div
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        
+        const productFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+     
+        //Insertar img en figure
+        productFigure.appendChild(productImgCard);
+     
+        //Insertar div y figure en div product-card
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productFigure);
+     
+        //Insertar div product-info e img en product-card
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+     
+        //Insertar div product-car a div cards-container
+         cardsContainer.appendChild(productCard);
+     }
+}
+
+/*
+  <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+      <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+      </div>
+      <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+      </figure>
+    </div>
+*/
