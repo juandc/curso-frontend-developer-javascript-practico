7abf9c5f35d8ad2dce4ad0ec7cb423a12b71e8a4
peves-platzi <mjpeves2002@gmail.com>
2023-01-05 22:25:45 -0500

    Fusión del menú mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var menuEmail = document.querySelector('.navbar-email');
 var desktopMenu = document.querySelector('.desktop-menu');
+var menuHamburguer = document.querySelector('.menu');
+var mobileMenu = document.querySelector('.mobile-menu')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguer.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
