ded6f055ab8c8d2a848698901ebf99ba972de29a
peves-platzi <mjpeves2002@gmail.com>
2023-01-05 23:43:29 -0500

    Carrito de Compras

--- main.js
+++ main.js
@@ -2,16 +2,35 @@ var menuEmail = document.querySelector('.navbar-email');
 var desktopMenu = document.querySelector('.desktop-menu');
 var menuHamburguer = document.querySelector('.menu');
 var mobileMenu = document.querySelector('.mobile-menu')
-
+var menuCarrito = document.querySelector('.navbar-shopping-cart');
+var aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguer.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    if(!aside.classList.toggle('inactive')){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    if(!aside.classList.toggle('inactive')){
+        aside.classList.add('inactive');
+    }
+        mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+
+    if(!desktopMenu.classList.toggle('inactive')){
+        desktopMenu.classList.add('inactive');
+    }
+    if(!mobileMenu.classList.toggle('inactive')){
+        mobileMenu.classList.add('inactive');
+    } 
+        aside.classList.toggle('inactive');
 }
 
