f450485d485e377a4ce5153707c8f5b745720d42
Alejandro <alejobeb@gmail.com>
2023-01-11 22:49:42 -0500

    commit con imagenes

--- main.js
+++ main.js
@@ -70,16 +70,35 @@ productList.push({
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 productList.push({
-  name: "Pantalla",
+  name: "Montaza",
   price: 220,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://static.cotodigital3.com.ar/sitios/fotos/full/00198600/00198634.jpg?3.0.150n",
 });
 productList.push({
-  name: "Compu",
+  name: "Croquetas",
   price: 620,
   image:
-    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "https://flaxandkale.com/images/4dbb96c07a1fe2a105f2f945a4be6188_177990?s=7608aa5c3e4aa43a033396598aa13202&fm=png",
+});
+
+productList.push({
+  name: "Zucaritas",
+  price: 620,
+  image:
+    "https://media.informabtl.com/wp-content/uploads/2019/02/0750100804295L2-768x768.jpg",
+});
+
+productList.push({
+  name: "Postre",
+  price: 620,
+  image: "https://www.mimate.com.co/wenv/file_image.php?id=349&download=&jpg=1",
+});
+
+productList.push({
+  name: "Wafflera",
+  price: 620,
+  image: "https://m.media-amazon.com/images/I/717UOhdL6PL.jpg",
 });
 
 function renderProducts(arr) {
