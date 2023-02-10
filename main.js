21c58ecc80db495c62d4b2b39ea4a7601b6c381d
DrJuanch <juanjo88023@gmail.com>
2023-02-09 21:11:43 -0500

    Hamburguer menu

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHambIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHambIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
