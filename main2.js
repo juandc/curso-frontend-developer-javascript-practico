003b1b4b2986f1788e3c40bd4ea4530985d80dff
Oscar <oscar.jcarabali@gmail.com>
2023-02-20 20:31:43 -0500

    Se modifica el menu en mobile, se pone, se le agregan los estilos en css y se configura en javascript para que aparezca con un clic o desaparezca tambien. Falta configurar que en version mobile al dar clic en otro lado que no sea el menu hamburguesa, el menu mobile se desaparezca. Se intento usar mouseout pero este solo funciona con un evento señalador y en mobile este evento no existe.

--- main2.js
+++ main2.js
@@ -1,8 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamMenu.addEventListener('click', toggleHamMenu);
 
-function toggleDesktopMenu () {
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+
+    desktopMenu.addEventListener('mouseout', mouseDesktopMenu);
+}
+
+function mouseDesktopMenu() {
+    desktopMenu.classList.toggle('inactive')
+}
+
+function toggleHamMenu() {
+    mobileMenu.classList.toggle('inactive')
+
+    mobileMenu.addEventListener('mouseout', mouseoutHamMenu);
+}
+function mouseoutHamMenu () {
+    mobileMenu.classList.toggle('inactive')
 }
