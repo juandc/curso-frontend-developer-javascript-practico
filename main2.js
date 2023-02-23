74389dc67d0036f8bd13304bf3cce1abdcc48c3c
Oscar <oscar.jcarabali@gmail.com>
2023-02-22 21:00:19 -0500

    se adiere el menu aside de carrito de compras que se activa con el icono de menu de carrito de compras. Se configura JS para que los tres menus ya instalados se activen e inactiven al momento de abrir otro menu.

--- main2.js
+++ main2.js
@@ -1,12 +1,21 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamMenu = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamMenu.addEventListener('click', toggleHamMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 
     desktopMenu.addEventListener('mouseout', mouseDesktopMenu);
@@ -17,10 +26,35 @@ function mouseDesktopMenu() {
 }
 
 function toggleHamMenu() {
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
 
     mobileMenu.addEventListener('mouseout', mouseoutHamMenu);
 }
-function mouseoutHamMenu () {
+function mouseoutHamMenu() {
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const isDesktopMenu = desktopMenu.classList.contains('inactive')
+    const isMobileMenu = mobileMenu.classList.contains('inactive')
+
+    if (!isMobileMenu) {
+        mobileMenu.classList.add('inactive')
+    } else if (!isDesktopMenu) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+
+    /*     aside.addEventListener('mouseout', mouseoutCarritoIcon); */
+}
+
+function mouseoutCarritoIcon() {
+    aside.classList.toggle('inactive')
 }
