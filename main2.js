bb3eba042b04948961df342f543ef3735213dc71
Oscar <oscar.jcarabali@gmail.com>
2023-02-26 13:25:33 -0500

    Se configura la interaccion de todos los componenetes de menu y de carritos para que al abrir cualquiera de la interacciones se cierren todas las demas. Esto tanto en desktop y mobile

--- main2.js
+++ main2.js
@@ -4,27 +4,37 @@ const menuHamMenu = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const productDetailAddCar = document.querySelector('.product-detail-addCar')
+const productDetailClose = document.querySelector('.product-detail-close')
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamMenu.addEventListener('click', toggleHamMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailClose.addEventListener('click', closeProductDetailAddCar);
 
 function toggleDesktopMenu() {
     const isAsideClose = aside.classList.contains('inactive');
+    const isProductDetailAddCarClose = productDetailAddCar.classList.contains('inactive');
 
     if (!isAsideClose) {
-        aside.classList.add('inactive')
+        aside.classList.add('inactive');
+        
+    } else if (!isProductDetailAddCarClose) {
+        productDetailAddCar.classList.add('inactive')
     }
-
     desktopMenu.classList.toggle('inactive')
+
 }
 
 function toggleHamMenu() {
     const isAsideClose = aside.classList.contains('inactive');
+    const isProductDetailAddCarClose = productDetailAddCar.classList.contains('inactive');
 
     if (!isAsideClose) {
         aside.classList.add('inactive')
+    } else if (!isProductDetailAddCarClose) {
+        productDetailAddCar.classList.add('inactive')
     }
 
     mobileMenu.classList.toggle('inactive')
@@ -33,14 +43,27 @@ function toggleHamMenu() {
 function toggleCarritoAside() {
     const isDesktopMenu = desktopMenu.classList.contains('inactive')
     const isMobileMenu = mobileMenu.classList.contains('inactive')
+    const isProductDetailAddCarClose = productDetailAddCar.classList.contains('inactive');
 
     if (!isMobileMenu) {
         mobileMenu.classList.add('inactive')
     } else if (!isDesktopMenu) {
         desktopMenu.classList.add('inactive')
+    } else if (!isProductDetailAddCarClose) {
+        productDetailAddCar.classList.add('inactive')
     }
 
     aside.classList.toggle('inactive')
+}
+
+function openProductDetailAddCar() {
+    aside.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    productDetailAddCar.classList.remove('inactive')
+}
+
+function closeProductDetailAddCar() {
+    productDetailAddCar.classList.add('inactive')
 }
 
 const productList = [];
@@ -76,13 +99,14 @@ productList.push({
             </div> */
 
 //se utiliza una funcion para acoger el codigo que crea, organiza y muestra el html de los productos
-function renderProducts (arrs) {
+function renderProducts(arrs) {
     for (product of arrs) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
         //imagen del producto
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image)
+        productImg.addEventListener('click', openProductDetailAddCar)
         //info del producto
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -97,16 +121,16 @@ function renderProducts (arrs) {
         productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
         //insertando elementos a bloques
         productInfoFigure.appendChild(productImgCart);//img en figure
-    
+
         productInfoDiv.appendChild(productInfoPrice);//precio en div
         productInfoDiv.appendChild(productInfoName);// nombre en div
-    
+
         productInfo.appendChild(productInfoDiv); //div en product-info
         productInfo.appendChild(productInfoFigure);//figure en product-info
-    
+
         productCard.appendChild(productImg);//img a product-card
         productCard.appendChild(productInfo);//product-info a product-card
-    
+
         cardsContainer.appendChild(productCard);//metemos todo en el div contenedor en el html
     }
 }
