5c1d686f42c9122f2389e082d07062c53be45233
CARLOS REINALDO GARCIA <kalliche16@gmail.com>
2023-03-07 04:49:57 -0500

    integración carrito de compras con menu de compras

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartContainer);
 
 
 function toggleDesktopMenu() {
   // cierra el menu del carrito al abrir menu de cuentas
-  const isAsideClosed = aside.classList.contains('inactive');
-  if(!isAsideClosed) {
-    aside.classList.toggle('inactive');
+  const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
+  if(!isshoppingCartContainerClosed) {
+    shoppingCartContainer.classList.toggle('inactive');
   }
   desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
   // ocultar menu de produnctos lista
-  const isAsideClosed = aside.classList.contains('inactive');
-  if(!isAsideClosed) {
-    aside.classList.add('inactive');
+  const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
+  if(!isshoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add('inactive');
   }
 
   mobileMenu.classList.toggle('inactive');
 }
-function toggleCarritoAside() {
+function toggleCarritoshoppingCartContainer() {
   // ocultar menu de producto con img vs lista de productos
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
   if(!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
   }
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
 }
 
 // Creacion de productos de la pagina
