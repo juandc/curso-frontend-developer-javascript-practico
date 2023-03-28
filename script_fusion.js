c0777b819750f3fe75dd0ed0d10857c86b39b5a8
JustinElMas <justin7b.jjr2014@gmail.com>
2023-03-28 02:09:22 -0500

    Add the shopping cart and learn conditionals ifconfig

--- script_fusion.js
+++ script_fusion.js
@@ -3,17 +3,25 @@ const $loginContainerActive = document.querySelector('.navbar-email');
 //esta es mi clase que tengo que llamar para desktop menu que le ponga o le quite a mi elemento
 const $desktopMenu = document.querySelector('.desktop-menu');
 const $burguerMenu = document.querySelector('.menu');
+const $shoppingCar = document.querySelector('.navbar-shopping-cart');
 const $mobileMenu = document.querySelector('.mobile-menu');
 
+const asideElement = document.querySelector('.product-detail');
 
 
 function activeLogin (e) {
 
     // $desktopMenu.classList.toggle('inactive');
 
-    //??? contains (e.targer) ??
+    //??? contains (e.target) ??
 
     if ($loginContainerActive.contains(e.target)) {
+
+        const isAsideClose = asideElement.classList.contains('inactive');
+
+        if (!isAsideClose) {
+            asideElement.classList.add('inactive');
+        };
 
         $desktopMenu.classList.toggle('inactive');
         console.log('Navbar');
@@ -36,10 +44,29 @@ function activeLogin (e) {
 // $loginContainerActive.addEventListener('click', activeLogin); //showmenu
 document.addEventListener('click', activeLogin);
 $burguerMenu.addEventListener('click', toogleMobileMenu);
+$shoppingCar.addEventListener('click', toogleCarAside);
 
 function toogleMobileMenu () {
 
+    const isAsideClose = asideElement.classList.contains('inactive');
+
+    if (!isAsideClose) {
+        asideElement.classList.add('inactive');
+    }
+
     $mobileMenu.classList.toggle('inactive');
+
+};
+
+function toogleCarAside () {
+
+    const isMobileMenuClosed = $mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        $mobileMenu.classList.add ('inactive');
+    }
+
+    asideElement.classList.toggle('inactive');
 
 };
 /*
