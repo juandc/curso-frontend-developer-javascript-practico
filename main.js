0149f263392c170d1258d25988374a1ad95796c5
pedrolcorderor <pedrolcorderor@gmail.com>
2023-04-01 16:21:10 -0400

    commit con contexto

--- main.js
+++ main.js
@@ -14,30 +14,43 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive');
+    shoppingCartContainer.classList.add("inactive");
   }
-  
-  desktopMenu.classList.toggle('inactive');
+
+  desktopMenu.classList.toggle("inactive");
+  /* primero le asignamos una variable a la palabra que pulsaremos (menuEmail) despues le asignamos
+  una evento click y que ejecute una funcion. esta funcion le pondremos esto
+      (  desktopMenu.classList.toggle('inactive'); lo que hara es que le quitara o pondra la clase a nuestro elemento con su variable ya asignada . esta clase tendra display:none y si se le quita aparera block.
+
+)*/
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
   if (!isAsideClosed) {
-    shoppingCartContainer.classList.add('inactive'); 
+    shoppingCartContainer.classList.add("inactive");
   }
 
   closeProductDetailAside();
-  
-  mobileMenu.classList.toggle('inactive');
+
+  mobileMenu.classList.toggle("inactive");
+  /* primero le asignamos una variable a la palabra que pulsaremos (menuHamIcon) despues le asignamos
+  una evento click y que ejecute una funcion. esta funcion le pondremos a la variable que queremos que aparesca la propiedad classList junto con su funcion toggle) (  mobileMenu.classList.toggle("inactive"))
+  ; lo que hara es que le quitara o pondra la clase a nuestro elemento con su variable ya asignada . esta clase tendra display:none y si se le quita aparera block cada vez que pulse mobileMenu
+
+)*/
 }
 
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-  
+  const isdesktoktopmenu = desktopMenu.classList.contains("inactive");
+  if (!isdesktoktopmenu) {
+    desktopMenu.classList.add("inactive");
+  }
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
   }
@@ -49,6 +62,7 @@ function toggleCarritoAside() {
   }
   
   shoppingCartContainer.classList.toggle('inactive');
+  
 }
 
 function openProductDetailAside() {
@@ -76,12 +90,17 @@ productList.push({
   price: 620,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
-
+/* creamos nuestros propios arrays porque no tenemos base de datos en el caso de que si ,lo usariamos . */
 function renderProducts(arr) {
+  /* con el for hacemos referencia a cada uno de los elemento del array */
+
+  /* lo colocamos todo en una funcion si en el futuro usamos otro array y asi reutilizarlo */
   for (product of arr) {
+    /* ahora creamos un div y le agregamos una clase ya lista en el css */
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
-  
+  /*  creamos una imagen que seria la de nuestros productos , con setAttribute le agregamo el atributo src y hacemos referencia elemento array con nombre (product)
+  junto con la propiedad image que tendra la url de cada uno de las imagenes */
     // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
@@ -97,20 +116,20 @@ function renderProducts(arr) {
     const productName = document.createElement('p');
     productName.innerText = product.name;
   
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
+    productInfoDiv.append(productPrice, productName);
+    /* este el div que contiene precio y nombre */
   
     const productInfoFigure = document.createElement('figure');
     const productImgCart = document.createElement('img');
     productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-  
+    /* no es necesario poner la url porque solo usaremos un icono */
+    /* ahora le agregaremos a la figura la imagen  */
     productInfoFigure.appendChild(productImgCart);
+    /* div que contien el div con precios y name, el la figura contiene lo anterior hecho */
+    productInfo.append(productInfoDiv, productInfoFigure);
+    /* el div principal le agregamos la imagen y el otro div que contiene todo los elementos y sus ramas */
+    productCard.append(productImg, productInfo);
   
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfoFigure);
-  
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
   
     cardsContainer.appendChild(productCard);
   }
