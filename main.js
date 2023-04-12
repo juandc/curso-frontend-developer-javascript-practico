5d5a354bd097091d998e3db2deca19382ec002f9
Anderson Parra <Andersonparrac@gmail.com>
2023-04-11 21:29:23 -0400

    Se arregló ingles de subtitulo (clase1.html) y se cambio source de imagenes (pantalla (linea de codigo 72) y compu ( linea de codigo 77) en main.js

--- main.js
+++ main.js
@@ -69,12 +69,12 @@ productList.push({
 productList.push({
   name: 'Pantalla',
   price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=600',
 });
 productList.push({
   name: 'Compu',
   price: 620,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://images.pexels.com/photos/400678/pexels-photo-400678.jpeg?auto=compress&cs=tinysrgb&w=600',
 });
 
 function renderProducts(arr) {
