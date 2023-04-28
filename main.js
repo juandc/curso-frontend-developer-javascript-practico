d98cf76fa4168843239d529bebcfb75f83f4178c
marlon <marlon.mosquerar@gmail.com>
2023-04-28 13:31:43 -0500

    mobile menu hamburguesa integrado

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-console.log(navbarEmail,
-  desktopMenu);
+const menuHamburguesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 navbarEmail.addEventListener('click',() =>{
   desktopMenu.classList.toggle('inactive')
-})
+});
+
+menuHamburguesa.addEventListener('click',() =>{
+  mobileMenu.classList.toggle('inactive')
+});
