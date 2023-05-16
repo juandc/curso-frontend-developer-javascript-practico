d65855669f565e97f44ed81e0a1cf22fb9981077
Juan Barriga <juanbarrigab2001@gmail.com>
2023-05-16 07:05:35 -0500

    Cambios en clases

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')  //modifica la classe 'inactive'
+    //El método toggle permite cada vez que se ejecute cambiar de estado la visibilidad del elemento HTML, es decir si está visible pasa a oculto y si se encuentra oculto pasa a visible.
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')  
 }
