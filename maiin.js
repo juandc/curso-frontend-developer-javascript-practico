cd39313074fd4f23cdddb375ac8964f94480b09f
Corazón Jaime <corazonjaime4@gmail.com>
2023-06-19 20:35:08 -0600

    se unieron los componentes mediante JS

--- maiin.js
+++ maiin.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu') 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart'); 
+const productDetailCloseIcon = document.querySelector(".product-detail-close"); 
 const menuHamIcon = document.querySelector(".menu"); 
 const mobileMenu = document.querySelector(".mobile-menu"); 
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu (){
 const isAsideClosed = shoppingCartContainer.classList.contains("inactive"); 
@@ -26,7 +29,8 @@ function toggleMobileMenu() {
    if (!isAsideClosed) {
      shoppingCartContainer.classList.add("inactive");
    }
-
+    closeProductDetailAside();
+    
     mobileMenu.classList.toggle('inactive');
 } 
 
@@ -37,8 +41,23 @@ function toggleCarritoAside() {
         mobileMenu.classList.add('inactive');
     }
 
+     const isProductDetailClosed = productDetailContainer.classList.contains("inactive");
+
+     if (!isProductDetailClosed) {
+       productDetailContainer.classList.add("inactive");
+     }
+
   shoppingCartContainer.classList.toggle("inactive");
 } 
+
+function openProductDetailAside(){
+shoppingCartContainer.classList.add('inactive')
+productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+  productDetailContainer.classList.add("inactive");
+}
 
 const productList = [];
 productList.push({
@@ -80,6 +99,7 @@ function renderProducts(array){
     // Product= {name, price, image} -> product.image
     const productImg = document.createElement("img");
     productImg.setAttribute("src", product.Image);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");
