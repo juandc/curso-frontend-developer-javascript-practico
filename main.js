191ff08bf56093b899b7ab2d7e6ed125c49eac21
Dani <danielsolerart@gmail.com>
2023-07-30 19:01:11 +0200

    Segundo Avance

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+  if (!isAsideClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+  
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() 
+{
+  mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+ 
+ 
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+  aside.classList.toggle('inactive');
 }
