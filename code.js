cfaeca53a729e5e63ea2d26e10c58ca27de2958a
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2023-08-09 12:17:56 -0400

    mobile menu and product list menu added

--- code.js
+++ code.js
@@ -1,9 +1,49 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuButton = document.querySelector('.menu');
+const menuProductDetail = document.querySelector('.product-detail');
+const shoppingCartButton = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuButton.addEventListener('click', toggleMobileMenu);
+shoppingCartButton.addEventListener('click', toggleMenuProductDetail);
 
 function toggleDesktopMenu()
 {
+    const isMenuProductDetail = menuProductDetail.classList.contains('inactive');
+
+    if(!isMenuProductDetail)
+    {
+        menuProductDetail.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu()
+{
+    const isMenuProductDetail = menuProductDetail.classList.contains('inactive');
+
+    if(!isMenuProductDetail)
+    {
+        menuProductDetail.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuProductDetail()
+{
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed)
+    {
+        mobileMenu.classList.add('inactive');
+    }
+    else if (!isDesktopMenuClosed)
+    {
+        desktopMenu.classList.add('inactive');
+    }
+
+    menuProductDetail.classList.toggle('inactive');
 }
