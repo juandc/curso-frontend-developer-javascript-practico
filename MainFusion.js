87edc701b0bdee10a0018c7af6b6a0d397a9773d
yoberty parada <yypara2@gmail.com>
2023-12-03 15:38:15 -0500

    se añadio el sub-menu movil al menu hamburguesa

--- MainFusion.js
+++ MainFusion.js
@@ -1,11 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu= document.querySelector('.desktop-menu');
+const menuHamburger= document.querySelector('.menu');
+const mobilmenu= document.querySelector('.mobile-menu');
 
 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburger.addEventListener('click', togglemobilmenu);//inicialmente se escucha cuando le den click al elemento en cuestión
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+    console.log('menuHamburger');
 }
+
+function togglemobilmenu() {
+    mobilmenu.classList.toggle('inactive')//se hace el llamado a la clase inactive la cual mediante la funsión toggle se le añade
+}
+
