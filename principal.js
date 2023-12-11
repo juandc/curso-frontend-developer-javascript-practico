cf2c0debb497c9c9b91ba79488dc24a4d653cf64
santiago Álvarez <santiagoaas10@gmail.com>
2023-12-11 12:48:38 -0500

    solucion carrito

--- principal.js
+++ principal.js
@@ -1,10 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail')
+
 const menuHamIcon= document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleDesktopMenuMobile);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+   
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive'); 
+    }
+    
+    aside.classList.toggle('inactive')
+}
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
