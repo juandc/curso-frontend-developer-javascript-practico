241066213d834550ff9466918d63787cc59beb36
CodexDei <jlacostc@gmail.com>
2024-01-06 17:58:43 -0500

    configuracion de aerturas y cierres de menu de escritorio y mobil

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const carritoIcon = document.querySelector(".navbar-shopping-cart");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toogleDesktopMenu);
+menuHamIcon.addEventListener("click", toogleMobileMenu);
+carritoIcon.addEventListener("click", toogleasideCarrito);
 
 function toogleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
+}
+
+function toogleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toogleasideCarrito() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.toggle("inactive");
+  }
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.toggle("inactive");
+  }
+
+  aside.classList.toggle("inactive");
 }
