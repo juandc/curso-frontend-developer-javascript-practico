71fe133be397ad72e358fe516dab5d0117af8b11
miguel <48770448+Miguel94666@users.noreply.github.com>
2024-04-17 20:25:28 -0700

    clase buena

---
+++ main2.js
@@ -0,0 +1,6 @@
+const navEmail =document.querySelector('.navbar-email');
+const desktop = document.querySelector('.desktop-menu')
+navEmail.addEventListener('click',toggleDesktopMenu)
+
+function toggleDesktopMenu(){
+desktop.classList.toggle('inactive')}
